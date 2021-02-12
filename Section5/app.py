4ed1b02b37fd5cec8ee5dca78acd8ef8233e65d4
Davi Pereira <davirp2009@gmail.com>
2021-02-12 12:13:24 -0300

    80. Logging in and retrieving Users from a database

--- Section5/app.py
+++ Section5/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask
 from flask_jwt import JWT
 from flask_restful import Resource, Api, reqparse
 
--- Section5/security.py
+++ Section5/security.py
@@ -1,19 +1,14 @@
 from werkzeug.security import safe_str_cmp
 
-from Section4.user import User
-
-users = [User(1, "bob", "asdf")]
-
-username_mapping = {u.username: u for u in users}
-userid_mapping = {u.id: u for u in users}
+from Section5.user import User
 
 
 def authenticate(username, password):
-    user = username_mapping.get(username)
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
 
 def identity(payload):
     user_id = payload["identity"]
-    return userid_mapping.get(user_id)
+    return User.find_by_id(user_id)
