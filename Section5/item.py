28568dfa8b6b5b22abf61cd97858aee772cb0019
Davi Pereira <davirp2009@gmail.com>
2021-02-12 15:39:01 -0300

    86. Refactoring insertion of items

--- Section5/item.py
+++ Section5/item.py
@@ -11,13 +11,6 @@ class Item(Resource):
         help="This field cannot be left blank!",
     )
 
-    # @jwt_required()
-    def get(self, name):
-        item = self.find_by_name(name)
-        if item:
-            return item
-        return {"message": "Item not found"}, 404
-
     @classmethod
     def find_by_name(cls, name):
         connection = sqlite3.connect("data.db")
@@ -31,14 +24,8 @@ class Item(Resource):
         if row:
             return {"item": {"name": row[0], "price": row[1]}}
 
-    # @jwt_required()
-    def post(self, name):
-        if self.find_by_name(name):
-            return {"message": f"An item with name '{name}' already exists."}, 400
-
-        data = Item.parser.parse_args()
-        item = {"name": name, "price": data["price"]}
-
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
 
@@ -47,6 +34,30 @@ class Item(Resource):
 
         connection.commit()
         connection.close()
+
+    # @jwt_required()
+    def get(self, name):
+        try:
+            item = self.find_by_name(name)
+        except Exception as err:
+            return {"message": f"An error occurred inserting the item.\nError: {err}"}, 500
+        else:
+            if item:
+                return item
+            return {"message": "Item not found"}, 404
+
+    # @jwt_required()
+    def post(self, name):
+        if self.find_by_name(name):
+            return {"message": f"An item with name '{name}' already exists."}, 400
+
+        data = Item.parser.parse_args()
+        item = {"name": name, "price": data["price"]}
+
+        try:
+            self.insert(item)
+        except Exception as err:
+            return {"message": f"An error occurred inserting the item.\nError: {err}"}, 500
 
         return item, 201
 
