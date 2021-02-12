67b58c640746848c1fccf48ac32fa7dc1335e43b
Davi Pereira <davirp2009@gmail.com>
2021-02-12 10:35:24 -0300

    76. Optimising our final code and request parsing

--- Section4/app.py
+++ Section4/app.py
@@ -15,6 +15,14 @@ items = []
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        "price",
+        type=float,
+        required=True,
+        help="This field cannot be left blank!",
+    )
+
     # @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x["name"] == name, items), None)
@@ -25,7 +33,8 @@ class Item(Resource):
         if next(filter(lambda x: x["name"] == name, items), None) is not None:
             return {"message": f"An item with name '{name}' already exists."}, 400
         # data = request.get_json(force=True)  # force: ignore header Content-Type and try to format to json
-        data = request.get_json(silent=True)  # silent: ignore header Content-Type and return null
+        # data = request.get_json(silent=True)  # silent: ignore header Content-Type and return null
+        data = Item.parser.parse_args()
         item = {"name": name, "price": data["price"]}
         items.append(item)
         return item, 201
@@ -39,14 +48,7 @@ class Item(Resource):
 
     # @jwt_required()
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument(
-            "price",
-            type=float,
-            required=True,
-            help="This field cannot be left blank!",
-        )
-        data = parser.parse_args()
+        data = Item.parser.parse_args()
 
         item = next(filter(lambda x: x["name"] == name, items), None)
         if item is None:
